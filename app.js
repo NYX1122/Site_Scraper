f86b8943db9a851456f8ca6685bc11990b17a531
BoDee Angus <bodee.angus@gmail.com>
2024-03-01 09:30:36 -0700

    I made it even better.

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 import puppeteer from 'puppeteer';
 
-export default async function (url) {
+export default async function (url, elementSelector) {
   try {
     const browser = await puppeteer.launch();
 
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: 'networkidle0', timeout: 60000 });
 
+    const data = await page.evaluate((selector) => {
+      const elementData = Array.from(document.querySelectorAll(selector));
+      return elementData.map((element) => {
+        if (element.innerText) {
+          return element.innerText;
+        } else {
+          return element;
+        }
+      });
+    }, elementSelector);
+
     await browser.close();
 
-    console.log('Site successfully scraped.');
-    return page;
+    console.log('Site_Scraper successfully scraped the site: ', data);
+
+    return data;
   } catch (error) {
     console.error('Site_Scraper encountered an error: ', error);
   }
--- appRunner.js
+++ appRunner.js
@@ -1,3 +1,3 @@
 import app from './app.js';
 
-app('https://bigfive-test.com/result/62e24ee8e7442a0009d95a1c');
+app('https://bigfive-test.com/result/62e24ee8e7442a0009d95a1c', 'p');
